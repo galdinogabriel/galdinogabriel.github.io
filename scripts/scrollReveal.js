dd09e2287aeca365e4d2fcdbab683b5f1685f386
Gabriel Matos <matosgabriel017@gmail.com>
2023-02-24 12:54:25 -0300

    add melhorias de responsividade e menu burger

--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -1,2 +1,10 @@
 ScrollReveal().reveal(".sobre-mim__paragrafo", { delay: 300, reset: true });
 ScrollReveal().reveal(".sobre-mim__nome", { delay: 200, reset: true });
+
+const nav = document.querySelector(".menu__nav")
+const burger = document.getElementById('burger');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
