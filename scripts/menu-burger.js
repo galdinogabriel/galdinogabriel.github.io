65671877291ec7d7448c564d44f8ac490eda30ec
Gabriel Matos <matosgabriel017@gmail.com>
2023-02-28 20:25:23 -0300

    animacoes melhoradas

--- scripts/menu-burger.js
+++ scripts/menu-burger.js
@@ -1,5 +1,59 @@
-ScrollReveal().reveal(".sobre-mim__paragrafo", { delay: 300, reset: true });
-ScrollReveal().reveal(".sobre-mim__nome", { delay: 200, reset: true });
+let leftToRight = {
+  origin: "left",
+  distance: "10%",
+  duration: 2000,
+  delay: 300,
+  reset: true,
+}
+let rigthToLeft = {
+  origin: "rigth",
+  distance: "10%",
+  duration: 1500,
+  delay: 500,
+  reset: true,
+}
+let bottomToTop = {
+  origin: "bottom",
+  distance: "20%",
+  duration: 2000,
+  delay: 500,
+  reset: true,
+  opacity:0,
+  scale: 1.2
+}
+let leftToRightSlow = leftToRight
+let bottomToTopslow = bottomToTop
+bottomToTopslow.duration = 2000
+leftToRightSlow.duration = 2000 
+
+
+ScrollReveal().reveal(".conteudo__paragrafo", leftToRight);
+ScrollReveal().reveal(".foto-perfil", bottomToTop);
+ScrollReveal().reveal(".sobre-mim__subtitulo", leftToRight);
+ScrollReveal().reveal(".sobre-mim__nome", leftToRight);
+ScrollReveal().reveal(".sobre-mim__card", leftToRight);
+ScrollReveal().reveal("#iportfolio", bottomToTop);
+ScrollReveal().reveal(".mensagem-contato h1", leftToRight);
+ScrollReveal().reveal(".apresentacao__info .apresentacao__msg", leftToRightSlow);
+ScrollReveal().reveal(".apresentacao__info .apresentacao__carreira", bottomToTopslow);
+ScrollReveal().reveal(".div-ilustration", bottomToTopslow);
+
+
+const itemsGrid = document.querySelectorAll('.skills-grid__item');
+
+itemsGrid.forEach(function(myElement) {
+  let bottomToTopGrid = bottomToTop;
+  bottomToTopGrid.delay = bottomToTopGrid.delay + 50
+  bottomToTopGrid.distance = '50px'
+  ScrollReveal().reveal(myElement, bottomToTopGrid);
+});
+
+
+
+
+
+
+
 
 const nav = document.querySelector(".menu__nav");
 const burger = document.getElementById("burger-menu");
