593aeaf8f089907f4aaa21cfc3c2614ca27503a7
galdinogabriel <matosgabriel017@gmail.com>
2023-03-10 21:21:05 -0300

    mudanca na cor dos icones das redes sociais e suavização das animacoes

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,30 +1,30 @@
 let leftToRight = {
     origin: "left",
     distance: "10%",
-    duration: 2000,
-    delay: 100,
+    duration: 3500,
+    delay: 50,
     reset: true,
   }
   let rigthToLeft = {
     origin: "rigth",
     distance: "10%",
-    duration: 1500,
-    delay: 100,
+    duration: 3500,
+    delay: 50,
     reset: true,
   }
   let bottomToTop = {
     origin: "bottom",
     distance: "20%",
-    duration: 2000,
-    delay: 300,
+    duration: 3500,
+    delay: 50,
     reset: true,
     opacity:0,
     scale: 1.2
   }
   let leftToRightSlow = leftToRight
   let bottomToTopslow = bottomToTop
-  bottomToTopslow.duration = 2000
-  leftToRightSlow.duration = 2000 
+  bottomToTopslow.duration = 3000
+  leftToRightSlow.duration = 3000 
   
   
   ScrollReveal().reveal(".conteudo__paragrafo", leftToRight);
@@ -37,6 +37,7 @@ let leftToRight = {
   ScrollReveal().reveal(".div-ilustration", bottomToTopslow);
   ScrollReveal().reveal(".mensagem-contato h1", leftToRightSlow);
   ScrollReveal().reveal(".projetos-grid__item--breve, .projetos-grid__item, .sobre-mim__nome", leftToRight);
+  ScrollReveal().reveal(".sobre-mim__nome", rigthToLeft);
   
   
   const itemsGrid = document.querySelectorAll('.skills-grid .skills-grid__item');
